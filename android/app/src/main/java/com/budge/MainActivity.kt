3cb1b1d3aa2b89af91d401c438b99d9682436253
Ivan Iankovskyi <ivaniankovskyi@gmail.com>
2024-03-24 19:46:37 -0700

    Build changes

--- android/app/src/main/java/com/budge/MainActivity.kt
+++ android/app/src/main/java/com/budge/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.budge
+package com.ibudge
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/budge/MainApplication.kt
+++ android/app/src/main/java/com/budge/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.budge
+package com.ibudge
 
 import android.app.Application
 import android.content.res.Configuration
